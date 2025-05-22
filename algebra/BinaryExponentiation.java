562875d26c09530c8dab8880e2a59a1bfc024088
Amresh Maurya <amreshbinary@gmail.com>
2025-05-22 19:32:56 +0530

    add binary expo

--- algebra/BinaryExponentiation.java
+++ algebra/BinaryExponentiation.java
@@ -1,14 +1,53 @@
+
 import java.util.Scanner;
+import java.math.BigInteger;
 
-public class BinaryExponentiation{
+public class BinaryExponentiation {
 
-public static Scanner sc = new Scanner(System.in);
+    public static Scanner sc = new Scanner(System.in);
+
+    public static BigInteger binpow(int base, long n) {
+
+        if (n == 0) {
+            return BigInteger.ONE;
+        }
 
 
-public static void main(String[] args) {
-	int a = sc.nextInt();
-    int n = sc.nextInt();
-   
-}
+        // result container
+        BigInteger result = BigInteger.ONE;
+
+        // make a is bigintger
+        BigInteger a = BigInteger.valueOf(base);
+
+        while(n > 0){
+
+             // odd
+             if(( n & 1 ) >= 1){
+
+                result = result.multiply(a) ;
+
+             }
+
+             // a * a
+             a = a.multiply(a);
+
+             // reduce power
+             n >>= 1;
+             
+
+
+        }
+
+return  result;
+
+    }
+
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int n = sc.nextInt();
+
+        System.out.println(binpow(a, n));
+
+    }
 
 }
