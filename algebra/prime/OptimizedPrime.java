edd29e92ab01ed06649ed261505ca20f92236735
Amresh Maurya <amreshbinary@gmail.com>
2025-05-25 22:18:53 +0530

    optimized prime

---
+++ algebra/prime/OptimizedPrime.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+
+public class OptimizedPrime {
+
+
+
+public static boolean isPrime(int n){
+
+
+    if (n <= 1) return false;
+    if (n <= 3) return true; // 2 and 3 are prime
+    if (n % 2 == 0 || n % 3 == 0) return false;
+
+    for (int i = 5;  i*i <= n; i+=2) {
+        if (n % i == 0) return false;
+    }
+
+    return true;
+
+
+}
+
+
+public static Scanner sc = new Scanner(System.in);
+public static void main(String[] args) {
+	
+	int time = sc.nextInt();
+
+	while(time-- > 0){
+
+int n = sc.nextInt();
+System.out.println(isPrime(n));
+
+	}
+
+}
+
+
+}
